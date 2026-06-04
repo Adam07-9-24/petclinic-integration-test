1736b1c75417220104fbcd12b13fa19b480e4f11
Calep Neyra <calep.neyra@tecsup.edu.pe>
2026-06-03 21:44:59 -0500

    Rama finalizada exitosamente

--- src/main/java/com/tecsup/petclinic/services/OwnerService.java
+++ src/main/java/com/tecsup/petclinic/services/OwnerService.java
@@ -1,15 +1,17 @@
 package com.tecsup.petclinic.services;
 
 import com.tecsup.petclinic.entities.Owner;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface OwnerService {
 
     Optional<Owner> findById(Long id);
-
     List<Owner> findByLastName(String lastName);
-
     List<Owner> findByCity(String city);
+
+    // --- MÉTODOS NUEVOS PARA EL INTEGRANTE A ---
+    Owner create(Owner owner);
+    Owner update(Owner owner);
+    void delete(Long id);
 }
